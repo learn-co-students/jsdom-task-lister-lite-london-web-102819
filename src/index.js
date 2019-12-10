68e7de03b0d98dfcd2c97401b34ce1360a88d5e4
Anastasia Pankina <siapankina@gmail.com>
2019-12-10 14:33:27 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
+
+
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  let form = document.getElementById("create-task-form");
+  form.addEventListener('submit', submitTask);
+
 });
+
+
+function submitTask(e) {
+  e.preventDefault();
+
+  const taskUl = document.getElementById("tasks");
+  const inputValue = document.getElementById("new-task-description").value
+  const button = document.createElement('button')
+  const li = document.createElement("li");
+
+
+  button.innerText = 'X'
+  li.innerText = inputValue
+  li.append(button)
+  button.addEventListener('click', function () { removeLi(li) })
+  taskUl.append(li);
+
+}
+
+function removeLi(li) {
+  li.remove()
+}

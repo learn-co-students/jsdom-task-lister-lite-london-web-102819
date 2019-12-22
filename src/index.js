d1b46b989b785aedb600513563eb3b7db33ea223
Qing <chingwang.qw@gmail.com>
2019-12-22 20:18:37 +0000

    revised

--- src/index.js
+++ src/index.js
@@ -5,20 +5,29 @@ document.addEventListener('DOMContentLoaded', () => {
 
 const listContainer = document.querySelector('#list')
 const ul = document.querySelector('#tasks')
-
+const priority = document.querySelector('select').value
 const submitForm = (event) => {
   event.preventDefault()
-  const TaskInput = document.querySelector('#new-task-description').value
+  const taskInput = document.querySelector('#new-task-description').value
   const deleteButton = document.createElement('button')
   deleteButton.innerText = 'X'
 
   const li = document.createElement('li')
-  li.innerText = TaskInput
+  li.innerText = taskInput
   li.appendChild(deleteButton)
   ul.appendChild(li)
 
-  deleteButton.addEventListener('click', function () { removeLi(li) });
+  if (priority === 'high') {
+    li.classList.add('high')
+  }
+  else if (priority === 'medium') {
+    li.classList.add('medium')
+  }
+  else {
+    li.classList.add('low')
+  }
 
+  deleteButton.addEventListener('click', function () { removeLi(li) });
 }
 
 const removeLi = (li) => {
